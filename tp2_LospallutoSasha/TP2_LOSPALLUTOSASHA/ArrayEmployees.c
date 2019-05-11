847eec8d56a5b40db1ff636f92c5ab1cc184ed65
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2019-05-10 23:30:03 -0300

    Add files via upload

---
+++ tp2_LospallutoSasha/TP2_LOSPALLUTOSASHA/ArrayEmployees.c
@@ -0,0 +1,190 @@
+//c
+#include "ArrayEmployees.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayEmployees.h"
+
+int initEmployees(Employee* list, int len)
+{
+    int resultado=-1;
+
+    for (int i=0;i<len;i++)
+    {
+        list[i].isEmpty=0;
+        //printf("%d\n\n", list[i].isEmpty);
+        resultado=0;
+    }
+ return resultado;
+}
+
+int buscarEspacio (Employee* list, int len)
+{
+    for (int i=0; i<len; i++)
+    {
+        if(list[i].isEmpty==0)
+        {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int buscarEspacioOcupado (Employee* list, int len)
+{
+    for (int i=0; i<len; i++)
+    {
+        if(list[i].isEmpty==1)
+        {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int addEmployee(Employee* list, int len, int id, char name[],char lastName[],float salary,int sector)
+{
+
+    for (int i=0; i<len; i++)
+    {
+        if(list[i].isEmpty==0)
+        {
+            strcpy(list[i].name,name);
+            strcpy(list[i].lastName,lastName);
+            list[i].id=i+1;
+            list[i].salary=salary;
+            list[i].sector=sector;
+            list[i].isEmpty=1;
+            printf("\n\nsu ID es: %d y posicion %d",list[i].id,i);
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int findEmployeeById(Employee* list, int len,int id)
+{
+    int index=-1;
+    for (int i=0;i<len;i++)
+    {
+        if (list[i].id==id && list[i].isEmpty==1)
+        {
+            index=i;
+            return index;
+        }
+    }
+ return index;
+}
+
+
+int removeEmployee(Employee* list, int len, int id)
+{
+    int r;
+
+    r=findEmployeeById(list,len,id);
+
+    if (r==-1)
+    {
+        return r;
+    }
+    else
+    {
+        list[r].isEmpty=0;
+        return r;
+    }
+
+}
+
+int sortEmployees(Employee* list, int len, int order)
+{
+    Employee listAux;
+
+    int resultado=0;
+
+    if (order==1)
+    {
+        for (int i=0; i<len-1; i++)
+        {
+            for (int j=i+1; j<len; j++)
+            {
+                if (strcmp(list[i].lastName,list[j].lastName)>0)
+                {
+                    if(list[i].isEmpty == 1) //aca verifico cual es el apellido de forma ascendente
+                    {
+                        listAux=list[i];
+                        list[i]=list[j];
+                        list[j]=listAux;
+                        resultado=1;
+
+                        if (strcmp(list[i].lastName,list[j].lastName)<=0) //verifico si son iguales
+                        {
+                            if (list[i].sector<list[j].sector) //verifico los sectores
+                            {
+                                listAux=list[i];
+                                list[i]=list[j];
+                                list[j]=listAux;
+                                resultado=1;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+    if (order==0)
+    {
+        for (int i=0; i<len-1; i++)
+        {
+            for (int j=i+1; j<len; j++)
+            {
+                if (strcmp(list[i].lastName,list[j].lastName)<0)
+                {
+                    if(list[i].isEmpty == 1) //aca verifico cual es el apellido de forma descendente
+                    {
+                        listAux=list[j];
+                        list[j]=list[i];
+                        list[i]=listAux;
+                        resultado=1;
+
+                        if (strcmp(list[i].lastName,list[j].lastName)>=0) //verifico si son iguales
+                        {
+                            if (list[i].sector>list[j].sector) //verifico los sectores
+                            {
+                                listAux=list[i];
+                                list[i]=list[j];
+                                list[j]=listAux;
+                                resultado=1;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    return resultado;
+
+}
+
+int printEmployees(Employee* list, int length)
+{
+    printf("   Id    Nombre    Apellido     Salario     Sector\n\n");
+
+    for(int i=0; i < length; i++)
+    {
+
+        if( list[i].isEmpty == 1)
+        {
+            printf("%4d %10s %10s     %6.2f %10d %10d\n", list[i].id, list[i].name, list[i].lastName, list[i].salary, list[i].sector, i);
+        }
+    }
+
+    return 0;
+}
+
+
---
+++ tp2_LospallutoSasha/TP2_LOSPALLUTOSASHA/main.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayEmployees.h"
+#include <string.h>
+
+int main()
+{
+    char seguir='s';
+    int opcion;
+    int r;
+    int n;
+    char nombre[51];
+    char apellido [51];
+    float salario;
+    int sector;
+    int id=0;
+    int aux;
+    int manera;
+
+    Employee Eemplo[ELEMENTS];
+
+    r=initEmployees(Eemplo,ELEMENTS);
+
+
+    while (seguir=='s')
+    {
+        //system("clear"); //LIMPIA PANTALLA EN LINUX
+        system("cls"); //LIMPIA PANTALLA EN WINDOWS
+        fflush( stdin ); //LIMPIA BUFFER WINDOWS
+        //__fpurge(stdin); //LIMPIA BUFFER LINUX
+        printf("\t\tMENU");
+        printf("\n\n1-Alta de nuevo usuario");
+        printf("\n\n2-Modificar usuario");
+        printf("\n\n3-Baja");
+        printf("\n\n4-Informar");
+        printf("\n\n5-Salir\n");
+
+        do
+        {
+            printf("\n\t\tingrese opcion: ");
+            scanf("\n%d",&opcion);
+        }
+        while (opcion<1 || opcion>5);
+
+        switch (opcion)
+        {
+        case 1:
+
+            r=buscarEspacio(Eemplo,ELEMENTS);
+
+            if (r==0)
+            {
+                //system("clear"); //LIMPIA PANTALLA EN LINUX
+                system("cls"); //LIMPIA PANTALLA EN WINDOWS
+                printf("\t\tCARGA DE EMPLEADO");
+                fflush(stdin);
+                printf("\n\nIngrese nombre: ");
+                gets(nombre);
+                fflush(stdin);
+                printf("\nIngrese apellido: ");
+                gets(apellido);
+                fflush(stdin);
+                printf("\nIngrese salario: ");
+                scanf("%f",&salario);
+                printf("\nIngrese sector: ");
+                scanf("%d",&sector);
+
+                r=addEmployee(Eemplo,ELEMENTS,id,nombre,apellido,salario,sector);
+
+                printf("\ncarga exitosa!\n\n");
+            }
+            else
+            {
+                printf("El numero de empleados cargados llego al maximo\n\n");
+            }
+
+            //system("pause");
+
+            break;
+
+        case 2:
+
+            //system("clear"); //LIMPIA PANTALLA EN LINUX
+            system("cls"); //LIMPIA PANTALLA EN WINDOWS
+
+            r=buscarEspacioOcupado(Eemplo,ELEMENTS);
+
+            if (r==-1)
+            {
+                printf("Debe ingresar un empleado primero\n");
+            }
+            else
+            {
+                printf("ingrese ID del empleado que desea modificar");
+                scanf("%d",&id);
+                aux=findEmployeeById(Eemplo,ELEMENTS,id);
+                if (aux==-1)
+                {
+                    printf("\nNo se encontro ningun empleado con ese ID\n");
+                }
+                else
+                {
+                    //system("clear"); //LIMPIA PANTALLA EN LINUX
+                    system("cls"); //LIMPIA PANTALLA EN WINDOWS
+                    printf("\t\tOPCIONES DE MODIFICACION");
+                    printf("\n\n1-Nombre");
+                    printf("\n\n2-Apellido");
+                    printf("\n\n3-Salario");
+                    printf("\n\n4-Sector");
+
+                    printf("\n\tingrese opcion: ");
+                    scanf("\n%d",&opcion);
+
+                    switch (opcion)
+                    {
+                    case 1:
+
+                        fflush(stdin);
+                        printf("\n\nIngrese el nombre nuevo: ");
+                        gets(nombre);
+                        strcpy(Eemplo[aux].name,nombre);
+                        printf("\n\n\nNuevo nombre agregado correctamente");
+                        break;
+                    case 2:
+
+                        fflush(stdin);
+                        printf("\n\nIngrese el apellido nuevo: ");
+                        gets(apellido);
+                        strcpy(Eemplo[aux].lastName,apellido);
+                        printf("\n\n\nNuevo apellido agregado correctamente");
+                        break;
+
+                    case 3:
+
+                        printf("\n\nIngrese el nuevo salario: ");
+                        scanf("%f",&salario);
+                        Eemplo[aux].salary=salario;
+                        printf("\n\n\nNuevo salario guardado correctamente");
+                        break;
+
+                    case 4:
+
+                        printf("\n\nIngrese el nuevo sector: ");
+                        scanf("%d",&sector);
+                        Eemplo[aux].sector=sector;
+                        printf("\n\n\nNuevo sector guardado correctamente");
+                        break;
+                    }
+
+                }
+
+            }
+
+            system("pause");
+            //printf("Press 'Enter' to continue: ... ");
+            //while ( getchar() != '\n');
+            break;
+
+        case 3:
+
+            //system("clear"); //LIMPIA PANTALLA EN LINUX
+            system("cls"); //LIMPIA PANTALLA EN WINDOWS
+
+            r=buscarEspacioOcupado(Eemplo,ELEMENTS);
+
+            if (r==-1)
+            {
+                printf("Debe ingresar un empleado primero\n");
+            }
+            else
+            {
+                printf("\nIngrese el ID del empleado que desea eliminar: ");
+                scanf("%d",&id);
+                r=removeEmployee(Eemplo,ELEMENTS,id);
+                if (r==-1)
+                {
+                    printf("\nEl id del empleado no existe");
+                }
+                else
+                {
+                    printf("\n El empleado ha sido eliminado correctamente");
+                }
+            }
+
+            system("pause");
+            //printf("Press 'Enter' to continue: ... ");
+            //while ( getchar() != '\n');
+            break;
+
+        case 4:
+
+
+
+            r=buscarEspacioOcupado(Eemplo,ELEMENTS);
+
+            if (r==-1)
+            {
+                printf("Debe ingresar un empleado primero\n");
+            }
+            else
+            {
+                printf("\n\topciones de orden");
+                printf("\n1-Ascendente");
+                printf("\n0-Descendente");
+                printf("\n\tIngrese opcion: ");
+                scanf("%d",&manera);
+
+                n=sortEmployees(Eemplo,ELEMENTS,manera);
+
+                n=printEmployees(Eemplo,ELEMENTS);
+            }
+
+            system("pause");
+            //printf("Press 'Enter' to continue: ... ");
+            //while ( getchar() != '\n');
+            break;
+
+        case 5:
+
+            seguir='c';
+
+            break;
+        }
+    }
+
+}
